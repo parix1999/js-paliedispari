6025bcc918b55e9674464d9b64d71f0470ebf383
parix1999 <parisottoedoardo@gmail.com>
2021-06-10 00:11:21 +0200

    js-6.0

--- js/script.js
+++ js/script.js
@@ -16,35 +16,36 @@
 
 // secondo esercizio pari o dispari: 
 
-var pariDispari = prompt('La somma di due numeri è pari o dispari ?');
-var numeroUtente = parseInt(prompt('Inserisci la tua scelta numero da 1 a 5'));
-
-outPut = document.getElementById('result');
-
-var randomUtente = numeroUtente ;  
-
-
 function randomNumber(min, max){
     var risultato = Math.floor(Math.random() * (max - min + 1) + min);
     return risultato;
 }
-
-var randomComputerN = randomNumber(1,5);
-
-outPut.innerHTML = 'Scelta utente: ' + randomUtente + '<br> Scelta computer: ' + randomComputerN;
-
-var sommaNumeri = parseInt(randomUtente) + parseInt(randomComputerN) ;
-
 function separiSeDispari (num) {
     if (parseInt(num) % 2 === 0) {
-        var numero = 'La somma dei due è pari!: ' + num; 
+        var numero = 'la somma dei due numeri è pari!: ' + num; 
     }else{
-        numero = 'La somma dei due è dispari! : ' + num;
+        numero = 'la somma dei due numeri è dispari! : ' + num;
     }
     return numero; 
 }
 
+var pariDispari = prompt('La somma di due numeri è pari o dispari ?');
+var numeroUtente = parseInt(prompt('Inserisci la tua scelta numero da 1 a 5'));
+outPut = document.getElementById('result');
+
+var randomUtente = numeroUtente ;  
+var randomComputerN = randomNumber(1,5);
+outPut.innerHTML = 'Scelta utente: ' + randomUtente + '<br> Scelta computer: ' + randomComputerN;
+
+var sommaNumeri = parseInt(randomUtente) + parseInt(randomComputerN) ;
 var scopertaPariDispari = separiSeDispari(sommaNumeri);
 
-outPut.innerHTML += '<br>' + scopertaPariDispari; 
+var pari = 'pari';
 
+if (pariDispari === pari && scopertaPariDispari % 2 === 0) {
+    var check = 'hai vinto ' + scopertaPariDispari;
+}else{
+    check = 'no ha vinto il computer ' + scopertaPariDispari;
+}
+
+outPut.innerHTML += '<br>' + check; 
