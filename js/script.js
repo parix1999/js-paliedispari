a83cbe077aaf5cae2ded832d1944e1836d32ec1f
parix1999 <parisottoedoardo@gmail.com>
2021-06-09 23:58:40 +0200

    js-4.0

--- js/script.js
+++ js/script.js
@@ -1,21 +1,45 @@
-var stringaUtente = prompt('Inserisci una parola per sapere se è Palidroma');
+// var stringaUtente = prompt('Inserisci una parola per sapere se è Palidroma');
 
-function palidroma(parola) {
-    // var lunghezzaParola = parola.lenght;
-    // var stringa = "";
-    // for (var i = (lunghezzaParola - 1); i >= 0; i--) {
-    //     stringa += parola[i];
-    // }
-    // return stringa;
-    var stringa = parola.split('').reverse().join('');
-    if (stringa === parola) {
-        alert('è palindroma');
-    }else{
-        alert('no');
-    }
-    return stringa;
+// function palidroma(parola) {
 
+//     var stringa = parola.split('').reverse().join('');
+//     if (stringa === parola) {
+//         alert('è palindroma');
+//     }else{
+//         alert('no');
+//     }
+//     return stringa;
+
+// }
+
+// alert(palidroma(stringaUtente));
+
+// secondo esercizio pari o dispari: 
+
+var pariDispari = prompt('La somma di due numeri è pari o dispari ?');
+var numeroUtente = parseInt(prompt('Inserisci la tua scelta numero da 1 a 5'));
+
+outPut = document.getElementById('result');
+
+var randomUtente = numeroUtente ;  
+
+
+function randomNumber(min, max){
+    var risultato = Math.floor(Math.random() * (max - min + 1) + min);
+    return risultato;
 }
 
-alert(palidroma(stringaUtente));
+var randomComputerN = randomNumber(1,5);
+
+outPut.innerHTML = 'Scelta utente: ' + randomUtente + '<br> Scelta computer: ' + randomComputerN;
+
+var sommaNumeri = parseInt(randomUtente) + parseInt(randomComputerN) ;
+
+
+if (parseInt(sommaNumeri) % 2 === 0) {
+    var numero = 'La somma dei due è pari!: ' + sommaNumeri; 
+}else{
+    numero = 'La somma dei due è dispari! : ' + sommaNumeri;
+}
+outPut.innerHTML += '<br>' + numero; 
 
